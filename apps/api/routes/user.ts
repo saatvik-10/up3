3c83b5096cd3e690755d59bb5a54295a7fd04d9a
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-16 22:32:03 +0530

    added status route

--- apps/api/routes/user.ts
+++ apps/api/routes/user.ts
@@ -5,7 +5,7 @@ import { prismaClient } from 'db/client';
 const route = Router();
 
 route.post('/website', authMiddleware, async (req, res) => {
-  const userId = req.userId!;
+  const userId = req.userId as string;
   const { url } = req.body;
 
   const data = await prismaClient.website.create({
@@ -19,7 +19,21 @@ route.post('/website', authMiddleware, async (req, res) => {
   });
 });
 
-route.get('/website/status', authMiddleware, async (req, res) => {});
+route.get('/website/status', authMiddleware, async (req, res) => {
+  const websiteId = req.query.websiteId as string;
+  const userId = req.userId as string;
+
+  const data = await prismaClient.website.findFirst({
+    where: {
+      id: websiteId,
+      userId,
+    },
+    include: {
+      ticks: true,
+    },
+  });
+  res.json(data);
+});
 
 route.get('/websites', authMiddleware, async (req, res) => {});
 
