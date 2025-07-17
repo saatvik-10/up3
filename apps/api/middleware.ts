d2af5663ac60fac28a948b533ae96cb821945602
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-17 17:16:31 +0530

    added jwt

--- apps/api/middleware.ts
+++ apps/api/middleware.ts
@@ -1,13 +1,21 @@
 import type { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 export function authMiddleware(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const authHeader = req.headers['authorization'];
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
 
-  req.userId = '1';
+  const decoded = jwt.verify(token, process.env.JWT_PUBLIC_CLERK_KEY!);
+  if (!decoded || !decoded.sub) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
 
+  req.userId = decoded.sub as string;
   next();
 }
