44a9179fc5e6ad21077e4c6bfbe26d38d382bce5
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-17 17:00:52 +0530

    added cors

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import userRoutes from './routes/user';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use('/api/v1', userRoutes);
 
 app.listen(8080, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 8080');
 });
