bdb62290c65bda05714a1e474a639f232a5a7db1
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-18 15:11:42 +0530

    updated schema

---
+++ packages/db/prisma/migrations/20250718093149_changed_status_enum/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [Up,Down] on the enum `WebsiteStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "WebsiteStatus_new" AS ENUM ('Good', 'Bad');
+ALTER TABLE "WebsiteTick" ALTER COLUMN "status" TYPE "WebsiteStatus_new" USING ("status"::text::"WebsiteStatus_new");
+ALTER TYPE "WebsiteStatus" RENAME TO "WebsiteStatus_old";
+ALTER TYPE "WebsiteStatus_new" RENAME TO "WebsiteStatus";
+DROP TYPE "WebsiteStatus_old";
+COMMIT;
